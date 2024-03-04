129cbc5190af30f535e190718a7b3eb5af7f19a1
rakeshkhaarol <kharol64666@gmail.com>
2024-03-04 11:52:49 +0530

    c2

--- src/components/NevBar.js
+++ src/components/NevBar.js
@@ -1,30 +1,55 @@
 import React from 'react'
+import {  Container,  Nav,  Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export const NevBar = () => {
   return (
     <>
-        <nav>
-            <Link to={'/'}>Home page</Link>
-            <ul>
-                <li>
-                    <Link to={'/create'}>RestaurantCreate</Link>
-                </li>
-                <li>
-                    <Link to={'/update'}>RestaurantUpdate</Link>
-                </li>
-                <li>
-                    <Link to={'/detail'}>RestaurantDetail</Link>
-                </li>
-                <li>
-                    <Link to={'/list'}>RestaurantList</Link>
-                </li>
-                <li>
-                    <Link to={'/search'}>RestaurantSearch</Link>
-                </li>
-               
-            </ul>
-        </nav>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="/">Resto</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1"><Link to={'/'}>Home</Link></Nav.Link>
+              <Nav.Link href="#action1"><Link to={'/create'}>Create</Link></Nav.Link>
+              <Nav.Link href="#action2"><Link to={'/update'}>Update</Link></Nav.Link>
+              <Nav.Link href="#action2"><Link to={'/list'}>List</Link></Nav.Link>
+              <Nav.Link href="#action2"><Link to={'/search'}>Search</Link></Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+
+
+
+      {/* <nav>
+        <Link to={'/'}>Home page</Link>
+        <ul>
+          <li>
+            <Link to={'/create'}>RestaurantCreate</Link>
+          </li>
+          <li>
+            <Link to={'/update'}>RestaurantUpdate</Link>
+          </li>
+          <li>
+            <Link to={'/detail'}>RestaurantDetail</Link>
+          </li>
+          <li>
+            <Link to={'/list'}>RestaurantList</Link>
+          </li>
+          <li>
+            <Link to={'/search'}>RestaurantSearch</Link>
+          </li>
+
+        </ul>
+      </nav> */}
     </>
   )
 }
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,16 +1,47 @@
 //1. import area
 import React from 'react'
+import { Table } from 'react-bootstrap'
 
 //2. definetion area
 function RestaurantList() {
     //hooks area
 
     //function definetion area
+    fetch('http://localhost:1337/api/restaurants')
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data)
+    })
+    .catch(err=>console.log(err))
 
 
     //return statments
     return (
-        <div>Restaurant List</div>
+        <>
+            <h1 className='text-center text-bg-danger'>Restaurant List</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Email</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    
+                </tbody>
+            </Table>
+        </>
     )
 }
 
