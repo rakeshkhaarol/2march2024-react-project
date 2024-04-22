c213a213f82d4a548efbae15662b674020884ae8
rakeshkhaarol <kharol64666@gmail.com>
2024-04-22 08:29:20 +0530

    expands

--- src/components/Nevbar.js
+++ src/components/Nevbar.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../src/img/Screenshot2.png';
 import { Link } from 'react-router-dom';
 
 export default function Nevbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-#FFFFFF border p-0">
+      <nav className="navbar navbar-expand-lg bg-body-#FFFFFF border p-0" onClick={toggleMenu}>
         <div className="container-fluid">
           <a className="navbar-brand fs-2" href="#"><img src={logo} alt="Logo" className="navbar-brand m-0" style={{ height: '60px', marginRight: '5px' }} /></a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" onClick={toggleMenu}>
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className={`collapse navbar-collapse  ${isMenuOpen ? 'show' : ''}`}>
             <div className="navbar-nav ms-auto">
               <Link className="nav-link active" to={'/'}>Home</Link>
               <Link className="nav-link" to={'/about'}>about</Link>
@@ -20,7 +26,8 @@ export default function Nevbar() {
             </div>
           </div>
         </div>
-      </nav >
+      </nav>
+   
     </>
   );
 }
